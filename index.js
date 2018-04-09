458c561f24f994089a1f89d71030cf64eff48f3f
Alex Jaynes <dr.ocheyo@gmail.com>
2018-04-09 16:59:51 -0700

    something smells wrong

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ class TicTacToe {
   prompt() {
     console.log(`it is ${this.player}'s turn...\nPlease input number of chosen square`);
     let response = readline();
-    handler(response)
+    this.handler(response);
   }
 
-  handler(i) {
-    this.board[i].value = turn;
+  handler(i, turn) {
+    this.board.chosen(i, turn);
+    this.check();
   }
 
   check() {
-
+    //check
+    this.board.draw();
+    this.prompt();
   }
 }
 
@@ -24,16 +27,17 @@ class Board {
     this.squares = Array(9).map((e, i) => new Square(i));
   }
 
-  draw() {
+  chosen(i) {
+    this.squares[i].value = turn;
+  }
 
+  draw() {
+    console.log(`${this.squares.slice(0, 3).join(' ')}\n${this.squares.slice(3, 6).join(' ')}\n${this.squares.slice(6).join(' ')}`);
   }
 }
 
-class Square {
+const Square = function(i) {
   constructor(i) {
-    this.index = i;
     this.value = i;
   }
-
-
 }
